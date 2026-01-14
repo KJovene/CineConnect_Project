1b1f943cc229e73f266a7675ebee1426db6c3950
CamillePaillou <167968308+CamillePaillou@users.noreply.github.com>
2026-01-14 21:21:38 +0100

    :sparkles: feat: configuration tanstack router, tanstack query et creation dossier routes :sparkles:

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+const RootLayout = () => (
+  <QueryClientProvider client={queryClient}>
+    <div className="p-2 flex gap-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>{' '}
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </QueryClientProvider>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
