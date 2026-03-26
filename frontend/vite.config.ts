1fa8aef98d47ccc490e27c569f6f38fa27ed7f66
Jovené Kévin <kevin@MacBook-Pro-de-Jovene.local>
2026-03-26 13:44:06 +0100

    fix: chemin pour récupérer le dossier shared

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@cineconnect/shared": path.resolve(__dirname, "../shared/src/index.ts"),
     },
   },
   optimizeDeps: {
